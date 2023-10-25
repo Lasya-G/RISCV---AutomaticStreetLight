f6bea9406cb9cf52969d129d9813b8d791f81a21
Lasya-G <140998582+Lasya-G@users.noreply.github.com>
2023-10-25 17:55:34 +0530

    Update automaticstreetlight.c

--- automaticstreetlight.c
+++ automaticstreetlight.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 int main()
 {
@@ -6,43 +6,61 @@ int pir_value;
 int ldr_value;
 int control;
 int control_reg;
+int mask = 0xFFFFFFF7;
+
+
+asm volatile(
+                "andi x30, x30, 0x0000\n\t"
+	    	:
+	    	: 
+		: "x30" 
+		);
 
 while(1)
-{
-asm volatile(
-            "or %0, x30, 1\n\t"
-            :"=r"(pir_value)
-        );
 
 asm volatile(
-            "or %0, x30, 1\n\t"
-            : "=r"(ldr_value)
-        );
+		"andi %0, x30, 0x01\n\t"
+		: "=r" (pir_value)
+		:
+		:);
+		
+		
+asm volatile(
+		"andi %0, x30, 0x02\n\t"
+		: "=r" (ldr_value)
+		:
+		:);         
 
 if (((pir_value)&&(ldr_value)) == 1)
 {
 control = 1;
-control_reg = control * 16;
+control_reg = control * 8;
 asm volatile(
-		"and x30, x30, %0 \n\t"
-		"and x30, x30, %1\n\t"		
+		"and x30, x30, %1 \n\t"
+		"or x30, x30, %0\n\t"		
 		:
-		:"r"(control), "r"(mask)
+		:"r"(control_reg), "r"(mask)
 		:"x30"
 		);
+		
+     	
+printf("control= %d\n", control);
+printf("Light is ON\n");
 }
 
 else
 {
 control = 0;
-control_reg = control *16;
+control_reg = control *8;
 asm volatile(
-		"and x30, x30, %0 \n\t"
-		"and x30, x30, %1\n\t"		
+		"and x30, x30, %1 \n\t"
+		"or x30, x30, %0\n\t"		
 		:
-		:"r"(control), "r"(mask)
+		:"r"(control_reg), "r"(mask)
 		:"x30"
-		);
+		)
+printf("control= %d\n", control);
+printf("Light is OFF\n");
 }
 }
 return 0;
